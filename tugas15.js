5f0bd58088618663c91748418d5cd30387a2cd7a
sonny faristrianto <sonnyf4r15@gmail.com>
2024-01-18 20:13:39 +0700

    tambahkan tugas15

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+var a = 8
+var b = 6
+var c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+
+
+console.log('nilai A = ',a)
+console.log('nilai B = ',b)
+console.log('Panjang sisi AC pada segitiga siku-siku tersebut adalah =',c,' cm')
